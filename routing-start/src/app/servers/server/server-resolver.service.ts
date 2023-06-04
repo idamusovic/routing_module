3970816cd15404046341c696c92bdba0a63026b2
Ida Musovic <idamusovic99@gmail.com>
2023-06-04 17:16:14 +0200

    updated

--- routing-start/src/app/servers/server/server-resolver.service.ts
+++ routing-start/src/app/servers/server/server-resolver.service.ts
@@ -1,8 +1,25 @@
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
+import { ServersService } from "../servers.service";
+import { Injectable } from "@angular/core";
 
-export class ServerResolver implements Resolve<{id: number, name: string, status: string}> {
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): { id: number; name: string; status: string; } | Observable<{ id: number; name: string; status: string; }> | Promise<{ id: number; name: string; status: string; }> {
-        return
+interface Server {
+    id: number;
+    name: string;
+    status: string;
+}
+
+@Injectable()
+export class ServerResolver implements Resolve<Server> {
+
+    constructor(
+        private serversService: ServersService
+    ) { }
+
+
+    resolve(
+        route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot): Server | Observable<Server> | Promise<Server> {
+        return this.serversService.getServer(route.params['id']);
     }
 }
